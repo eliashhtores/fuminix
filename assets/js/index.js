f694cdb8b8397c5d51e73205e4055f672f92a083
eliashhtores <33284999+eliashhtores@users.noreply.github.com>
2020-11-24 10:31:03 -0600

    Added improvement

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,133 +1,72 @@
-const services = {
-    "smallRoaches":
-        [
-            {
-                "price": 599,
-                "description": "<strong>Cucarachas (Chicas)</strong><br>$599"
-            }
-        ],
-    "bedBugs":
-        [
-            {
-                "price": 599,
-                "description": "<strong>Chinches de cama</strong><br>$599"
-            }
-        ],
-
-    "bigRoaches":
-        [
-            {
-                "price": 499,
-                "description": "<strong>Cucarachas (Grandes)</strong><br>$499"
-            }
-        ],
-    "various":
-        [
-            {
-                "price": 499,
-                "description": "<strong>Alacranes/Arañas/Hormigas</strong><br>$499"
-            }
-        ],
-    "flyingBugs":
-        [
-            {
-                "price": 499,
-                "description": "<strong>Insectos voladores</strong><br>$499"
-            }
-        ],
-    "none":
-        [
-            {
-                "price": 399,
-                "description": "<strong>Ninguno, solo preventivo</strong><br>$399"
-            }
-        ],
-    "smallHouse":
-        [
-            {
-                "price": 0,
-                "description": "<strong>Chica</strong><br>1 piso<br>1-2 habitaciones<br>Menos de 60 m<br>$0*"
-            }
-        ],
-    "mediumHouse":
-        [
-            {
-                "price": 100,
-                "description": "<strong>Mediana</strong><br>2 pisos<br>2-3 habitaciones<br>Menos de 80 m<br>$100*"
-            }
-        ],
-    "bigHouse":
-        [
-            {
-                "price": 200,
-                "description": "<strong>Grande</strong><br>2 pisos<br>3-4 habitaciones<br>Menos de 300 m<br>$200*"
-            }
-        ],
-    "veryBigHouse":
-        [
-            {
-                "price": 300,
-                "description": "<strong>Muy grande</strong><br>2 o más pisos<br>3 o más habitaciones<br>Más de 300 m<br>$300 o más*"
-            }
-        ],
-
-    "veryClose":
-        [
-            {
-                "price": 0,
-                "description": "<strong>Muy cerca</strong><br>Menos de 3 km<br>$0"
-            }
-        ],
-    "close":
-        [
-            {
-                "price": 50,
-                "description": "<strong>Cerca</strong><br>Menos de 7 km<br>$50"
-            }
-        ],
-    "far":
-        [
-            {
-                "price": 100,
-                "description": "<strong>Lejos</strong><br>Menos de 15 km<br>$100"
-            }
-        ],
-    "veryFar":
-        [
-            {
-                "price": 150,
-                "description": "<strong>Muy lejos</strong><br>En las afueras o en otra ciudad<br>$150 o más**"
-            }
-        ]
-};
-let service = {};
+const total = document.querySelector('#total');
+const totalParagraph = document.querySelector('#totalParagraph');
+const description = document.querySelector('#description');
+const forms = document.querySelectorAll('form');
+const form = forms[0];
 const plague = document.querySelector('#plague');
 const houseSize = document.querySelector('#houseSize');
 const distance = document.querySelector('#distance');
-const total = document.querySelector('#total');
+
+$("#plagueRow button").click(function () {
+    if ($(this)[0].id === 'none') {
+        $(this).parent().siblings().children().removeClass('active');
+    }
+    const none = $(this).parent().siblings().children()[4];
+    $(none).removeClass('active');
+});
+
+$("#houseRow button, #distanceRow button").click(function () {
+    $(this).parent().siblings().children().removeClass('active');
+});
+
+// @TODO refactor this to replace the above jQuery code
+// document.querySelectorAll('#plagueRow button').forEach(item => {
+//     item.addEventListener('click', (e) => {
+//         for (let sibling of e.target.parentElement.parentElement.children) {
+//             if (sibling !== e.target.parentElement) {
+//                 if (sibling.firstElementChild.id === 'none') {
+//                     sibling.firstElementChild.classList.remove('active');
+//                 }
+//             } else {
+//                 console.log(sibling.firstElementChild.id);
+//             }
+//         }
+//     });
+// });
 
 document.querySelectorAll('.btn').forEach(item => {
     item.addEventListener('click', e => {
-        delete service[e.target.name];
-        service[e.target.name] = services[e.target.id];
-        try {
-            service["plague[]"][0].description !== undefined;
-            plague.innerHTML = `- ${service["plague[]"][0].description}`;
-        } catch (e) {
-        }
-        try {
-            service["houseSize[]"][0].description !== undefined;
-            houseSize.innerHTML = `- ${service["houseSize[]"][0].description}`;
-        } catch (e) {
-        }
-        try {
-            service["distance[]"][0].description !== undefined;
-            distance.innerHTML = `- ${service["distance[]"][0].description}`;
-        } catch (e) {
-        }
-        try {
-            total.innerHTML = service["plague[]"][0].price + service["houseSize[]"][0].price + service["distance[]"][0].price;
-        } catch (e) {
+        e.target.blur();
+        if (e.target.classList.contains('active'))
+            e.target.classList.remove('active');
+        else
+            e.target.classList.add('active');
+        e.preventDefault();
+    });
+});
+
+document.querySelector('#quote').addEventListener('click', (e) => {
+    let accumulator = 0;
+
+    plague.innerText = '';
+    Array.from(form.elements).forEach((element) => {
+        if (element.classList.contains('active') && element.hasAttribute('value')) {
+            accumulator += parseInt(element.value);
+            if (element.parentElement.parentElement.id == 'plagueRow') {
+                plague.innerHTML += `${element.innerText} <br>`;
+            } else if (element.parentElement.parentElement.id == 'houseRow') {
+                houseSize.innerText = element.innerText;
+            } else
+                distance.innerText = element.innerText;
         }
     });
+
+    total.innerText = accumulator;
+    plague.classList.remove('d-none');
+    houseSize.classList.remove('d-none');
+    distance.classList.remove('d-none');
+    totalParagraph.classList.remove('d-none');
+    description.classList.remove('d-none');
+
+    e.preventDefault();
 });
